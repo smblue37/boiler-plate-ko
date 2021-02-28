a886b013965b7b500286b88512c51bc298a49789
smblue37 <72495266+smblue37@users.noreply.github.com>
2021-03-01 05:50:29 +0900

    로그인/회원가입/인증체크

--- server/index.js
+++ server/index.js
@@ -24,6 +24,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/api/hello', (req, res) => {
+  res.send("안녕하세요 ~ ")
+})
+
 app.post('/api/users/register', (req, res) => {
 
   //회원가입할 때 필요한 정보들을 client에서 가져오면
